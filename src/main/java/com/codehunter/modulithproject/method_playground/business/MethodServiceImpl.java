249ba079f4cbc0d4f9a0eec1f5882308a7a93cd3
codehunter <hunterx6323@gmail.com>
2023-12-17 16:22:43 +0700

    feat: Method | change to use RestClient

--- src/main/java/com/codehunter/modulithproject/method_playground/business/MethodServiceImpl.java
+++ src/main/java/com/codehunter/modulithproject/method_playground/business/MethodServiceImpl.java
@@ -5,30 +5,28 @@ import com.codehunter.modulithproject.method_playground.mapper.EntityMapper;
 import com.codehunter.modulithproject.method_playground.method.MethodEntity;
 import com.codehunter.modulithproject.method_playground.method.MethodResponse;
 import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 
 import java.util.LinkedList;
 import java.util.List;
 
 @Service
 public class MethodServiceImpl {
-    private final String methodBaseUrl;
-    private final RestTemplate methodClient;
+    private final RestClient methodRestClient;
 
-    public MethodServiceImpl(@Value("${app.method.url}") String methodBaseUrl, RestTemplate methodClient) {
-        this.methodBaseUrl = methodBaseUrl;
-        this.methodClient = methodClient;
+    public MethodServiceImpl(RestClient methodRestClient) {
+        this.methodRestClient = methodRestClient;
     }
 
     @RegisterReflectionForBinding({MethodEntity.class, LinkedList.class, MethodResponse.class})
     public List<Entity> getAllEntities() {
-        ResponseEntity<MethodResponse<List<MethodEntity>>> response = methodClient.exchange(methodBaseUrl + "/entities", HttpMethod.GET, null, new ParameterizedTypeReference<MethodResponse<List<MethodEntity>>>(){});
-        MethodResponse<List<MethodEntity>> body = response.getBody();
+        MethodResponse<List<MethodEntity>> body = methodRestClient.get()
+                .uri("/entities")
+                .retrieve()
+                .body(new ParameterizedTypeReference<MethodResponse<List<MethodEntity>>>() {
+                });
         List<MethodEntity> data = body.data();
         return data.stream().map(EntityMapper::toEntity).toList();
     }
--- src/main/java/com/codehunter/modulithproject/method_playground/config/MethodClientConfig.java
+++ src/main/java/com/codehunter/modulithproject/method_playground/config/MethodClientConfig.java
@@ -3,20 +3,21 @@ package com.codehunter.modulithproject.method_playground.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Collections;
+import org.springframework.web.client.RestClient;
 
 @Configuration
 public class MethodClientConfig {
     private final String authorizationHeaderName = "Authorization";
     @Value("${app.method.api.token}")
     private String authorizationHeaderValue;
+    @Value("${app.method.url}")
+    private String methodBaseUrl;
 
     @Bean
-    public RestTemplate methodClient() {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setInterceptors(Collections.singletonList(new CustomHeaderInterceptor(authorizationHeaderName, "Bearer " + authorizationHeaderValue)));
-        return restTemplate;
+    public RestClient methodRestClient() {
+        return RestClient.builder()
+                .defaultHeader(authorizationHeaderName, "Bearer " + authorizationHeaderValue)
+                .baseUrl(methodBaseUrl)
+                .build();
     }
 }
