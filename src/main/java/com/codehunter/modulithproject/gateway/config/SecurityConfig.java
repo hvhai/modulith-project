577b76a423aa38a6ae33f92625ff1bdde01e3c36
hvhai <hunterx6323@gmail.com>
2024-10-09 16:10:08 +0700

    feat: PERS-35 add security config to make server support MVC and api by okta

--- src/main/java/com/codehunter/modulithproject/gateway/config/SecurityConfig.java
+++ src/main/java/com/codehunter/modulithproject/gateway/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.codehunter.modulithproject.gateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,17 +14,22 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 
 @Configuration
-@EnableWebSecurity(debug = false)
+@EnableWebSecurity(debug = true)
 public class SecurityConfig {
     private static final String AUTHORITY_PREFIX = "ROLE_";
     private static final String CLAIM_ROLES = "http://coundowntimer.com/roles";
@@ -34,24 +41,48 @@ public class SecurityConfig {
             "/v3/**",
             "/swagger-resources/**",
             "/h2-console/**",
-            "/no-auth/**"
+            "/no-auth/**",
+            "/",
+            "/images/**"
     };
 
+    @Value("${okta.oauth2.issuer}")
+    private String issuer;
+    @Value("${okta.oauth2.client-id}")
+    private String clientId;
+
+
     @Bean
+    @Order(0)
     SecurityFilterChain securityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
         http
-                .securityMatcher(PUBLIC_RESOURCES)
-                .authorizeHttpRequests(auth -> auth.requestMatchers(PUBLIC_RESOURCES).permitAll())
                 .securityMatcher("/api/**")
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/*/admin/**").hasAnyRole("admin")
-                        .anyRequest().hasAnyRole("user", "admin"))
+                        .requestMatchers(PUBLIC_RESOURCES).permitAll()
+                        .requestMatchers("/api/*/admin/**").hasAuthority("admin")
+                        .requestMatchers("/api/**").hasAnyAuthority("user", "admin"))
+                .formLogin(AbstractHttpConfigurer::disable)
+                .oauth2Login(AbstractHttpConfigurer::disable)
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt
                                 .jwtAuthenticationConverter(getJwtAuthenticationConverter())))
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
         ;
+        return http.build();
+    }
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain mvcSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "/images/**", "/api/**").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(withDefaults())
+                .oauth2ResourceServer(AbstractHttpConfigurer::disable)
+                .logout(logout -> logout
+                        .addLogoutHandler(logoutHandler()));
         return http.build();
     }
 
@@ -77,5 +108,16 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    private LogoutHandler logoutHandler() {
+        return (request, response, authentication) -> {
+            try {
+                String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
+                response.sendRedirect(issuer + "v2/logout?client_id=" + clientId + "&returnTo=" + baseUrl);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
     }
 }
